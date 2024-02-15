f66c1b6be48f1f145acfc663057c0d29a6031b16
Mathieu <mathieunr@hotmail.fr>
2024-02-15 12:36:32 +0100

    route signin et signup

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/users');
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 
 module.exports = app;
---
+++ models/connexion.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connexionString = process.env.CONNEXION_STRING;
+
+mongoose.connect(connexionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
