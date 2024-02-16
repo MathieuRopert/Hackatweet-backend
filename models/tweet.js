67639591f3ccd72becfb45bda199d544f75beb70
Mathieu <mathieunr@hotmail.fr>
2024-02-16 17:11:41 +0100

    route des tweet

--- models/tweet.js
+++ models/tweet.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const tweetSchema = mongoose.Schema({
     date: Date,
     content: String,
-    Trend: String,
-    NumberLikes: Number,
+    Trend: [String],
+    NumberLikes: [Number],
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     
 
--- routes/tweet.js
+++ routes/tweet.js
@@ -5,32 +5,34 @@ const Tweet = require('../models/tweet')
 const User = require('../models/users');
 
 router.get('/', (req, res) => {
-Tweet.find().then(data => {
-res.json({ result: true, Tweet: data })
-});
+    Tweet.find().then(data => {
+        res.json({ result: true, Tweet: data })
+    });
 });
 
 router.post('/addtweet/:token', (req, res) => {
     let regExp = /\#\w*/g;
-    const trendTweet = Array.from(req.body.content.matchAll(regExp))
-User.findOne({token: req.params.token})
-.then(data => {
-    const newTweet = new Tweet ({
-        date: Date.now(),
-        content: req.body.content,
-        Trend: trendTweet,
-        NumberLikes: [],
-        user: data.id,
-    });
-    newTweet.save().then(newDoc => {
-        res.json({ result: true, trend: newDoc.Trend})
-    })
-})
+    const trendTweet = Array.from(req.body.content.matchAll(regExp)).map(match => match[0])
+    User.findOne({ token: req.params.token })
+        .then(data => {
+            console.log(data)
+            const newTweet = new Tweet({
+                date: Date.now(),
+                content: req.body.content,
+                Trend: trendTweet,
+                NumberLikes: [],
+                user: data.id,
+            });
+            newTweet.save().then(newDoc => {
+                res.json({ result: true, content: newDoc.content, trend: newDoc.Trend })
+                console.log('ok')
+            })
+        })
 });
 
 router.delete('/:tweets', (req, res) => {
-Tweet.deleteOne({tweets: req.params.id})
-res.json({result: true, message: 'Tweet as been delete '})
+    Tweet.deleteOne({ tweets: req.params.id })
+    res.json({ result: true, message: 'Tweet as been delete ' })
 });
 
 module.exports = router;
